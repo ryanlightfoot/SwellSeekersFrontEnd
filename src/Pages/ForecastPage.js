6357f6dde4bc11f5ba68e5c49c97db4d21cf3c04
ryanlight <ryanlightfoot509@gmail.com>
2023-10-23 21:10:43 +0200

    Fixed the location name problem

--- src/Pages/ForecastPage.js
+++ src/Pages/ForecastPage.js
@@ -5,23 +5,21 @@ import axios from 'axios';
 
 function ForecastPage() {
     const { location } = useParams();
-    const realLocation = location + 1;
+    const realLocation = parseInt(location) + 1;
     const [surfLocation, setSurfLocation] = useState(null);
 
 
     useEffect(() => {
         // Assuming your API endpoint for fetching a surf location by ID is something like '/api/surflocations/{locationId}'
-        axios.get(`https://localhost:7177/api/SurfLocation/${1}`)
+        axios.get(`https://localhost:7177/api/SurfLocation/${realLocation}`)
             .then((response) => {
-                console.log(response.data);
-                console.log(realLocation);
                 setSurfLocation(response.data);
-                console.log(response.data.name);
             })
             .catch((error) => {
                 console.error("Error fetching surf location: ", error);
             });
-    });
+    }, [location]);
+
 
   return (
     <div>
